4dbe0fa299f888b0faf056d1cda3078876037630
Peter Friese <peter@peterfriese.de>
2011-06-02 20:16:05 +0200

    Refactoring in order to be able to support speech input for input fields and textareas. Plus: change body language to German for xing.com to allow for easier input of German names.

--- extension/spk2srch.js
+++ extension/spk2srch.js
@@ -1,14 +1,36 @@
-$(document).ready(function(){
-	$('[type=text]').attr("x-webkit-speech", "");
-	$('[type=text]').bind("webkitspeechchange", function(field) {
-		console.log(field);
-		var fieldName = $(field.target).attr('name');
-		if (fieldName == 'q' || fieldName == 'search') {
-			field.target.form.submit();			
+$(document).ready(function() {
+	
+	/**
+	 * Augment the given field with speech input capabilities:
+	 */
+	function enableSpeech(inputElement) {
+		$(inputElement).attr('x-webkit-speech', '');
+		$(inputElement).bind('webkitspeechchange', function(field) {
+			var fieldname = $(field.target).attr('name');
+			if (fieldName == 'q' || fieldName == 'search') {
+				field.target.form.submit();
+			}
+		})		
+	}
+	
+	// augment all text input fields and search fields with speech input capabilities:
+	$('input').each(function(index, element) {
+		var type = element.type;
+		if (type == 'text' || type == 'search') {
+			enableSpeech(element);
 		}
 	});
-	$('[type=search]').attr("x-webkit-speech", "");
-	$('[type=search]').bind("webkitspeechchange", function(field) {
-		field.target.form.submit();
-	});	
+	
+	// speech input currently not available for textareas, but let's try anyway to be safe for the future:
+	$('textarea').each(function(index, element) {
+		enableSpeech(element);
+	});
+	
+	// special handling for xing.com:
+	if (window.location.hostname.indexOf('xing') >= 0) {
+		$('body').attr('lang', 'de');
+		$('html').attr('lang', 'de');
+		$('.usearchform button').css('right', '-16px');		
+	}
+	
 });
